da5bf14cdf494119b8efcc756dcb3eeeca84a5d2
Léo Meyne <78816569+LeoMeyne@users.noreply.github.com>
2026-02-03 20:38:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,4 +8,12 @@ window.addEventListener("load", (event) => {
         button.style.left = `${l}%`;
         button.style.top = `${t}%`;
     });
+
+    button.addEventListener('click', function () {
+        const currentHeight = parseInt(button.style.height) || 40;
+        const currentWidth = parseInt(button.style.width) || 150;
+        button.style.height = `${currentHeight - 10}px`;
+        button.style.width = `${currentWidth - 10}px`;
+        console.log('Nouvelle taille:', button.style.height, button.style.width);
+    });
 });
