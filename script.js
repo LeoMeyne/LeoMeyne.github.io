4e9c03efdcb272abcc5730b7fc509889a933679b
Leo Meyne <leo.meyne@coexya.eu>
2026-02-03 22:37:49 +0100

    update

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 window.addEventListener("load", (event) => {
     const button = document.getElementById("btn");
     button.addEventListener('mouseover', function () {
+        button.style.position = 'absolute';
         const l = Math.ceil(Math.random() * 90)
         const t = Math.ceil(Math.random() * 90)
         console.log(l, t)
